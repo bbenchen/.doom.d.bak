c9c415943fce1612a0529432bf597f1cac4e60d4
Mike Chen <517926804@qq.com>
2019-11-10 18:00:33 +0800

    Add tng feature for company

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
        chinese
 
        :completion
-       company                          ; the ultimate code completion backend
+       (company +tng)                   ; the ultimate code completion backend
        ;;helm                           ; the *other* search engine for love and life
        ;;ido                            ; the other *other* search engine...
        (ivy                             ; a search engine for love and life
