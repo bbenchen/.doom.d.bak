45314bb8e600aeef7e2fcb50b4b9c462ca8fe535
Mike Chen <517926804@qq.com>
2019-12-16 20:34:37 +0800

    Improve: consistent with ivy-posframe display

--- modules/tools/youdao/config.el
+++ modules/tools/youdao/config.el
@@ -7,7 +7,37 @@
         ;; Set file path for saving search history
         youdao-dictionary-search-history-file (concat doom-cache-dir "youdao")
         ;; Enable Chinese word segmentation support
-        youdao-dictionary-use-chinese-word-segmentation t))
+        youdao-dictionary-use-chinese-word-segmentation t)
+
+  (when (and (featurep! +childframe)
+             (featurep! :completion ivy +childframe))
+    (defun +youdao-dictionary--posframe-tip (string)
+      "Show STRING using posframe-show."
+      (unless (and (require 'posframe nil t) (posframe-workable-p))
+        (error "Posframe not workable"))
+
+      (let ((word (youdao-dictionary--region-or-word)))
+        (if word
+            (progn
+              (with-current-buffer (get-buffer-create youdao-dictionary-buffer-name)
+                (let ((inhibit-read-only t))
+                  (erase-buffer)
+                  (youdao-dictionary-mode)
+                  (insert (youdao-dictionary--format-result word))
+                  (goto-char (point-min))
+                  (set (make-local-variable 'current-buffer-word) word)))
+              (posframe-show youdao-dictionary-buffer-name
+                             :font ivy-posframe-font
+                             :background-color (face-attribute 'ivy-posframe :background nil t)
+                             :foreground-color (face-attribute 'ivy-posframe :foreground nil t)
+                             :internal-border-width ivy-posframe-border-width
+                             :internal-border-color (face-attribute 'ivy-posframe-border :background nil t))
+              (unwind-protect
+                  (push (read-event) unread-command-events)
+                (posframe-delete youdao-dictionary-buffer-name)))
+          (message "Nothing to look up"))))
+
+    (advice-add #'youdao-dictionary--posframe-tip :override #'+youdao-dictionary--posframe-tip)))
 
 (map! "C-x C-y" (if (featurep! +childframe)
                     #'youdao-dictionary-search-at-point-posframe
