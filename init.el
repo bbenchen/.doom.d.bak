fe97f6844c8c3de46970886959178030d7918796
Mike Chen <517926804@qq.com>
2020-01-13 12:08:28 +0800

    Add make module

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
        lsp
        (:if IS-MAC macos)               ; MacOS-specific commands
        magit                            ; a git porcelain for Emacs
-       ;;make                           ; run make tasks from Emacs
+       make                             ; run make tasks from Emacs
        ;;pass                           ; password manager for nerds
        ;;pdf                            ; pdf enhancements
        ;;prodigy                        ; FIXME managing external services & code builders
