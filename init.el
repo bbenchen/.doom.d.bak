c67613bc87bcf861204542182aab25a53e2a4b18
Mike Chen <517926804@qq.com>
2020-03-04 15:41:24 +0800

    Sync doom config

--- init.el
+++ init.el
@@ -37,9 +37,7 @@
        nav-flash                        ; blink the current line after jumping
        ;;neotree                        ; a project drawer, like NERDTree for vim
        ophints                          ; highlight the region an operation acts on
-       (popup                           ; tame sudden yet inevitable temporary windows
-        +all                            ; catch all popups that start with an asterix
-        +defaults)                      ; default popup rules
+       (popup +defaults)                ; tame sudden yet inevitable temporary windows
        ;;pretty-code                    ; replace bits of code with pretty symbols
        ;;tabs                           ; an tab bar for Emacs
        treemacs                         ; a project drawer, like neotree but cooler
@@ -60,7 +58,7 @@
        multiple-cursors                 ; editing in many places at once
        ;;objed                          ; text object editing for the innocent
        ;;parinfer                       ; turn lisp into python, sort of
-       rotate-text                      ; cycle region at point between text candidates
+       ;;rotate-text                    ; cycle region at point between text candidates
        snippets                         ; my elves. They type so I don't have to
        ;;word-wrap                      ; soft wrapping with language-aware indent
 
