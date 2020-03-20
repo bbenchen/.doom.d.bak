5523180e9e959023208ec8924ae2901516df0d83
Mike Chen <517926804@qq.com>
2020-03-20 18:30:33 +0800

    Update magit-todos config

--- modules/my/magit/config.el
+++ modules/my/magit/config.el
@@ -3,3 +3,9 @@
 ;; magit
 (after! magit
   (setq magit-revision-show-gravatars nil))
+
+;; magit-todos
+(if (executable-find "nice")
+    (setq magit-todos-nice t))
+(if (executable-find "rg")
+    (setq magit-todos-scanner 'magit-todos--scan-with-rg))
