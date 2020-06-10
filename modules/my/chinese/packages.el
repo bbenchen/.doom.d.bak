4e56676a7dce64e3dfa4eb1c6b6e8537b2950011
Mike Chen <517926804@qq.com>
2020-06-10 11:23:51 +0800

    Bump :my chinese

--- modules/my/chinese/packages.el
+++ modules/my/chinese/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; my/chinese/packages.el
 
-(package! rime :pin "daf956df30d4007f70a4495560d656e517ab69a9")
+(package! rime :pin "2a0b116d56bf54456eb5d6e8e80a7a6cf6944863")
 (when (featurep! +childframe)
   (package! posframe :pin "c4459028fbe6740315ff1ed6f37e8c4decd3d050"))
