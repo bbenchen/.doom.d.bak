4416554e80000ff2801e8a59b66185410e6c1ede
Mike Chen <517926804@qq.com>
2020-06-27 16:50:15 +0800

    Remove duplicate package

--- modules/my/scala/packages.el
+++
@@ -1,5 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; my/scala/packages.el
-
-(when (and (featurep! :tools lsp) (not (featurep! :tools lsp +eglot)))
-  (package! lsp-metals :pin "5468b638cd81b7d9ce9edc652c281b28bd775c23"))
