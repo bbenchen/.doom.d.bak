110d5e00d4fa5f30b527376b92ea391ace7f895a
Mike Chen <517926804@qq.com>
2020-06-28 13:18:33 +0800

    protobuf has supported imenu

--- modules/my/protobuf/config.el
+++ modules/my/protobuf/config.el
@@ -3,11 +3,4 @@
 (use-package! protobuf-mode
   :init
   (add-hook! 'protobuf-mode-hook
-    (defun setup-protobuf-imenu ()
-      "Setup imenu regex for protocol buffers."
-      (setq
-       imenu-generic-expression
-       '((nil "^[[:space:]]*\\(message\\|service\\|enum\\)[[:space:]]+\\([[:alnum:]]+\\)" 2)))))
-
-  (add-hook! 'protobuf-mode-hook
     (display-line-numbers-mode t)))
