9b20828e8ba4585173b65b7b4edfd8554239da99
Mike Chen <517926804@qq.com>
2020-06-28 13:21:28 +0800

    Bump protobuf

--- modules/my/protobuf/packages.el
+++ modules/my/protobuf/packages.el
@@ -3,4 +3,4 @@
 
 (package! protobuf-mode
   :recipe (:host github :repo "emacsmirror/protobuf-mode" :files ("*.el"))
-  :pin "94b7bd7e8b87ff100c603153d2f8d7a2a08ab50b")
+  :pin "34d2bec69ddec511ec2dd4a8108fb920c27efe11")
