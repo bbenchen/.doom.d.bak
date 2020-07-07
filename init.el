1c55b4e9792aaef98688dbf0606ec696d9f0015f
Mike Chen <517926804@qq.com>
2020-07-07 20:39:52 +0800

    Enable fold module

--- init.el
+++ init.el
@@ -49,8 +49,8 @@
 
        :editor
        ;;(evil +everywhere)             ; come to the dark side, we have cookies
-       file-templates                   ; auto-snippets for empty files
-       ;;fold                           ; (nigh) universal code folding
+       file-templates               ; auto-snippets for empty files
+       fold                         ; (nigh) universal code folding
        format                       ; automated prettiness
        lispy                        ; vim for lisp, for people who dont like vim
        multiple-cursors             ; editing in many places at once
