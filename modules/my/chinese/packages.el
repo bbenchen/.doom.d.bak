99fa4417124aca251284d031c8c70081a319a874
Mike Chen <517926804@qq.com>
2020-07-30 10:55:06 +0800

    Bump :my chinese translate

--- modules/my/chinese/packages.el
+++ modules/my/chinese/packages.el
@@ -3,4 +3,4 @@
 
 (package! rime :pin "345e8c5bd6ef6f381880c4c315506dc6a5bc4cca")
 (when (featurep! +childframe)
-  (package! posframe :pin "922e4d239f7a083213d856de67a9686a091b1e27"))
+  (package! posframe :pin "b3028b01a96699b1dfc9b2b5f4e3ba2bc1aa8317"))
--- modules/my/translate/packages.el
+++ modules/my/translate/packages.el
@@ -4,7 +4,7 @@
 (package! youdao-dictionary :pin "bd839711e3cf6d3eb9c308cea5694369a440899e")
 
 (when (featurep! +childframe)
-  (package! posframe :pin "922e4d239f7a083213d856de67a9686a091b1e27"))
+  (package! posframe :pin "b3028b01a96699b1dfc9b2b5f4e3ba2bc1aa8317"))
 
 (when (featurep! :completion company)
   (package! company-english-helper
