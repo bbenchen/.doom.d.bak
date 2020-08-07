e6ebeea975436e913c4b1e09eb42d982fd968f95
Mike Chen <517926804@qq.com>
2020-08-07 11:31:29 +0800

    Bump posframe

--- modules/my/chinese/packages.el
+++ modules/my/chinese/packages.el
@@ -3,4 +3,4 @@
 
 (package! rime :pin "345e8c5bd6ef6f381880c4c315506dc6a5bc4cca")
 (when (featurep! +childframe)
-  (package! posframe :pin "b3028b01a96699b1dfc9b2b5f4e3ba2bc1aa8317"))
+  (package! posframe :pin "6d604a71deb45295f7b6ff3f98e06aeece9888be"))
--- modules/my/translate/packages.el
+++ modules/my/translate/packages.el
@@ -4,7 +4,7 @@
 (package! youdao-dictionary :pin "bd839711e3cf6d3eb9c308cea5694369a440899e")
 
 (when (featurep! +childframe)
-  (package! posframe :pin "b3028b01a96699b1dfc9b2b5f4e3ba2bc1aa8317"))
+  (package! posframe :pin "6d604a71deb45295f7b6ff3f98e06aeece9888be"))
 
 (when (featurep! :completion company)
   (package! company-english-helper
