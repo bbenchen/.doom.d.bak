70c6c11762688c65f22ad1ef9e8bec1d5a437d17
Mike Chen <517926804@qq.com>
2020-09-06 21:44:24 +0800

    Bump: my chinese
    DogLooksGood/emacs-rime@aab2169 ->  DogLooksGood/emacs-rime@9d139b5

--- modules/my/chinese/packages.el
+++ modules/my/chinese/packages.el
@@ -1,6 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; my/chinese/packages.el
 
-(package! rime :pin "aab21695f1dee0db3ddf0f0c834b94aecec7057c")
+(package! rime :pin "9d139b56b45e91b787031f5b86c3462287d6ef29")
 (when (featurep! +childframe)
   (package! posframe :pin "b28210748d982ca1c30debb3b3861063e7223c6c"))
