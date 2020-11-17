3b7faf69ae3d8509ada8b3bea4a18d3fb1c62088
Mike Chen <517926804@qq.com>
2020-11-17 11:59:32 +0800

    Disable spell for xml

--- modules/my/spell/config.el
+++ modules/my/spell/config.el
@@ -2,3 +2,11 @@
 
 (after! ispell
   (setq ispell-dictionary "en_US"))
+
+(when (featurep! :checkers spell)
+  (after! nxml-mode
+    (add-hook! 'nxml-mode-hook
+      (defun disable-spell-for-xml()
+        (if (featurep! :checkers spell +flyspell)
+            (flyspell-mode -1)
+          (spell-fu-mode -1))))))
