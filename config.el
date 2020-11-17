1f8383be5cf8c90eb2aaa6895c8eae7bddf395a2
Mike Chen <517926804@qq.com>
2020-11-17 18:45:58 +0800

    Refactor && Format

--- config.el
+++ config.el
@@ -4,53 +4,13 @@
 
 ;; (setq warning-minimum-level :error)
 
-(setq-default fill-column 120)
-
 (setq user-full-name "Chen Xianbin"
       user-mail-address "517926804@qq.com"
       epa-file-encrypt-to user-mail-address
       auth-sources '("~/.authinfo.gpg"))
 
 (add-hook! 'emacs-startup-hook (setq default-directory "~/"
-                                     org-directory "~/Documents/org"
-                                     org-noter-notes-search-path '("~/Documents/org")))
-
-;; display line number
-(setq display-line-numbers-type 'relative)
-
-(when (display-graphic-p)
-  ;; fonts
-  (cond (IS-LINUX (setq doom-font (font-spec :family "Hack Nerd Font" :size 10)))
-        (IS-MAC (setq doom-font (font-spec :family "Hack Nerd Font" :size 12))))
-
-  ;; no broder
-  ;; (set-frame-parameter nil 'undecorated t)
-  ;; (add-to-list 'initial-frame-alist '(undecorated . t))
-  (add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-  ;; theme
-  (after! doom-themes
-    (setq doom-themes-treemacs-theme "doom-colors"))
-  (pcase (frame-parameter nil 'ns-appearance)
-    ('light (setq doom-theme 'doom-one-light))
-    (_ (setq doom-theme 'doom-one)))
-
-  (if (and IS-MAC
-           (boundp 'ns-system-appearance-change-functions))
-      (add-hook! 'ns-system-appearance-change-functions
-                 #'(lambda (appearance)
-                     (mapc #'disable-theme custom-enabled-themes)
-                     (pcase appearance
-                       ('light (load-theme 'doom-one-light t))
-                       ('dark (load-theme 'doom-one t)))))))
-
-;; frame
-(add-hook! 'after-make-frame-functions #'(lambda (frame)
-                                           (when (not (display-graphic-p frame))
-                                             (select-frame frame)
-                                             (doom/reload-theme))))
-
-(add-hook! 'after-init-hook #'windmove-default-keybindings)
+                                     org-directory "~/Documents/org"))
 
 ;; workspaces
 (setq +workspaces-on-switch-project-behavior t)
@@ -63,17 +23,3 @@
   (dolist (suffix '(".bak" ".exe"))
     (add-to-list 'projectile-globally-ignored-file-suffixes suffix)))
 
-;; org
-(after! org
-  ;; https://manateelazycat.github.io/emacs/2020/04/02/org-font.html
-  (defun org-buffer-face-mode-variable ()
-    (interactive)
-    (when (or IS-LINUX IS-MAC)
-      (make-face 'width-font-face)
-      (if IS-LINUX
-          (set-face-attribute 'width-font-face nil :font "Sarasa Mono SC 10")
-        (set-face-attribute 'width-font-face nil :font "Sarasa Mono SC 12"))
-      (setq buffer-face-mode-face 'width-font-face)
-      (buffer-face-mode)))
-
-  (add-hook! 'org-mode-hook #'org-buffer-face-mode-variable))
--- modules/my/ui/config.el
+++ modules/my/ui/config.el
@@ -1,5 +1,67 @@
 ;;; my/tools/config.el -*- lexical-binding: t; -*-
 
+(setq-default fill-column 120)
+
+;; display line number
+(setq display-line-numbers-type 'relative)
+
+(when (display-graphic-p)
+  ;; fonts
+  (cond (IS-LINUX (setq doom-font (font-spec :family "Hack Nerd Font" :size 10)))
+        (IS-MAC (setq doom-font (font-spec :family "Hack Nerd Font" :size 12))))
+
+  ;; no broder
+  ;; (set-frame-parameter nil 'undecorated t)
+  ;; (add-to-list 'initial-frame-alist '(undecorated . t))
+
+  ;; maximize the window
+  (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+  ;; theme
+  (after! doom-themes
+    (setq doom-themes-treemacs-theme "doom-colors"))
+
+  (cond (IS-MAC (pcase (frame-parameter nil 'ns-appearance)
+                  ('light (setq doom-theme 'doom-one-light))
+                  (_ (setq doom-theme 'doom-one)))
+                (if (boundp 'ns-system-appearance-change-functions)
+                    (add-hook! 'ns-system-appearance-change-functions
+                               #'(lambda (appearance)
+                                   (mapc #'disable-theme custom-enabled-themes)
+                                   (pcase appearance
+                                     ('light (load-theme 'doom-one-light t))
+                                     ('dark (load-theme 'doom-one t)))))))
+        (t (setq doom-theme 'doom-tomorrow-day)))
+
+  ;; org
+  ;; (after! org
+  ;;   ;; https://manateelazycat.github.io/emacs/2020/04/02/org-font.html
+  ;;   (defun org-buffer-face-mode-variable ()
+  ;;     (interactive)
+  ;;     (when (or IS-LINUX IS-MAC)
+  ;;       (make-face 'width-font-face)
+  ;;       (if IS-LINUX
+  ;;           (set-face-attribute 'width-font-face nil :font "Sarasa Nerd Font 10")
+  ;;         (set-face-attribute 'width-font-face nil :font "Sarasa Nerd Font 12"))
+  ;;       (setq buffer-face-mode-face 'width-font-face)
+  ;;       (buffer-face-mode)))
+
+  ;;   (add-hook! 'org-mode-hook #'org-buffer-face-mode-variable))
+  )
+
+;; frame
+(add-hook! 'after-make-frame-functions #'(lambda (frame)
+                                           (when (not (display-graphic-p frame))
+                                             (select-frame frame)
+                                             (doom/reload-theme))))
+
+;; modeline
+(after! doom-modeline
+  (setq doom-modeline-major-mode-icon t
+        doom-modeline-persp-name t
+        doom-modeline-enable-word-count t
+        doom-modeline-indent-info t))
+
 ;; centered-cursor
 (use-package! centered-cursor-mode
   :commands (centered-cursor-mode
@@ -9,10 +71,3 @@
         :desc "Centered point" "C--" #'centered-cursor-mode)
   :config
   (setq ccm-recenter-at-end-of-file t))
-
-;; modeline
-(after! doom-modeline
-  (setq doom-modeline-major-mode-icon t
-        doom-modeline-persp-name t
-        doom-modeline-enable-word-count t
-        doom-modeline-indent-info t))
