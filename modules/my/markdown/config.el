6d4ed9965f3c26ba88232a5b865d61f8ccb959c7
Mike Chen <517926804@qq.com>
2020-11-17 17:09:32 +0800

    Format

--- modules/my/markdown/config.el
+++ modules/my/markdown/config.el
@@ -1,14 +1,14 @@
 ;;; my/markdown/config.el -*- lexical-binding: t; -*-
 
 (add-hook! 'markdown-mode-hook
-    (defun flycheck-enable-markdownlint ()
-      "Set the `mardkownlint' config file for the current buffer."
-      (let* ((md-lint ".markdownlint.json")
-             (md-file buffer-file-name)
-             (md-lint-dir (and md-file
-                               (locate-dominating-file md-file md-lint))))
-        (setq-local flycheck-markdown-markdownlint-cli-config
-                    (expand-file-name (concat md-lint-dir md-lint))))))
+  (defun flycheck-enable-markdownlint ()
+    "Set the `mardkownlint' config file for the current buffer."
+    (let* ((md-lint ".markdownlint.json")
+           (md-file buffer-file-name)
+           (md-lint-dir (and md-file
+                             (locate-dominating-file md-file md-lint))))
+      (setq-local flycheck-markdown-markdownlint-cli-config
+                  (expand-file-name (concat md-lint-dir md-lint))))))
 
 (after! grip-mode
   (if (featurep 'xwidget-internal)
