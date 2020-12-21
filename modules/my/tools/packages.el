beb4d8471ade6bc9e1a66dbece7c1e92b82a331a
Mike Chen <517926804@qq.com>
2020-12-21 15:53:15 +0800

    Bump :my tools

--- modules/my/tools/packages.el
+++ modules/my/tools/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; my/tools/packages.el
 
-(package! exec-path-from-shell :pin "4ea306a76f0f8371053dd12d77a3b5b4438974d3")
+(package! exec-path-from-shell :pin "d14d6d2966efe5a1409f84a6b9d998268f74761d")
 (package! command-log-mode :pin "af600e6b4129c8115f464af576505ea8e789db27")
 (if IS-LINUX
     (package! eaf
