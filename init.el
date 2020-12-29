e6336d5f150d121cf26dda99b99bb181ae0a60e1
Mike Chen <517926804@qq.com>
2020-12-29 16:55:07 +0800

    Enable PDF module only in MacOS

--- init.el
+++ init.el
@@ -90,10 +90,10 @@
        ;;gist                           ; interacting with github gists
        lookup                           ; helps you navigate your code and documentation
        (lsp +peek)
-       magit                         ; a git porcelain for Emacs
-       make                          ; run make tasks from Emacs
+       magit                            ; a git porcelain for Emacs
+       make                             ; run make tasks from Emacs
        ;;pass                           ; password manager for nerds
-       pdf                           ; pdf enhancements
+       (:if IS-MAC pdf)                 ; pdf enhancements
        ;;prodigy                        ; FIXME managing external services & code builders
        ;;rgb                            ; creating color strings
        ;;terraform                      ; infrastructure as code
