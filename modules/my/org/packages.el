c85517546b0dd3462abd911055314a57d69d99d9
Mike Chen <517926804@qq.com>
2020-12-30 18:37:41 +0800

    Update valign download address

--- modules/my/org/packages.el
+++ modules/my/org/packages.el
@@ -1,4 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; my/org/packages.el
 
-(package! valign)
+(package! valign
+  :recipe (:host github :repo "casouri/valign")
+  :pin "1d2ecd2afdb77021eaffc213d9fbbc6d9c38ed82")
