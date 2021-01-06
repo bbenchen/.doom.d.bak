06baaa0f7fb7bc8cc37c362e49213e794f87fee9
Mike Chen <517926804@qq.com>
2021-01-06 11:38:38 +0800

    unpin vterm

--- packages.el
+++ packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-(unpin! (:completion ivy) (:ui doom) (:emacs ibuffer) (:checkers spell) (:tools magit debugger lsp) (:lang java scala))
+(unpin! (:completion ivy) (:ui doom) (:emacs ibuffer) (:checkers spell) (:term vterm) (:tools magit debugger lsp) (:lang java scala))
