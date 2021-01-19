11c91c0f2dad8b45af8d2c9ae98adb6e230e9ba7
Mike Chen <517926804@qq.com>
2021-01-19 12:35:57 +0800

    Update magit-svn address

--- modules/my/magit/packages.el
+++ modules/my/magit/packages.el
@@ -2,4 +2,6 @@
 ;;; my/magit/packages.el
 
 (if (executable-find "svn")
-  (package! magit-svn :pin "2cff1a30a30f2b3963342a7d185ec13fc12279c3"))
+  (package! magit-svn
+    :recipe (:host github :repo "cxb811201/magit-svn")
+    :pin "01af1e7c226d5b8db2c84f19e3c15e8d51a89d5b"))
