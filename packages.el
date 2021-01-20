8431ce6cc603e9175b9af549f32d0afe27909edf
Mike Chen <517926804@qq.com>
2021-01-21 00:15:45 +0800

    unpin company module

--- packages.el
+++ packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-(unpin! (:completion ivy) (:ui doom) (:emacs ibuffer) (:checkers spell) (:term vterm) (:tools magit debugger lsp) (:lang java scala))
+(unpin! (:completion company ivy) (:ui doom) (:emacs ibuffer) (:checkers spell) (:term vterm) (:tools magit debugger lsp) (:lang java scala))
