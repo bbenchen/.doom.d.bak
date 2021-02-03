e87866aa5bd2c6ff5ee88ad21ae0f8c08acc78a1
Mike Chen <517926804@qq.com>
2021-02-03 21:34:13 +0800

    Add dwm to eaf-wm-focus-fix-wms

--- modules/my/tools/config.el
+++ modules/my/tools/config.el
@@ -36,7 +36,7 @@
   :defer 2
   :custom
   (eaf-config-location (expand-file-name "eaf/" doom-etc-dir))
-  (eaf-wm-focus-fix-wms '("i3" "bspwm" "LG3D"))
+  (eaf-wm-focus-fix-wms '("i3" "bspwm" "dwm" "LG3D"))
   (eaf-find-alternate-file-in-dired t)
   (eaf-proxy-type "http")
   (eaf-proxy-host "127.0.0.1")
