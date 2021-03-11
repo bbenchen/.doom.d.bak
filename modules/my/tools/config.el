5d64c9f69275d34f9f3530df2bb72ed3456cb962
Mike Chen <517926804@qq.com>
2021-03-11 17:45:45 +0800

    exec-path-from-shell is only valid in MacOS

--- modules/my/tools/config.el
+++ modules/my/tools/config.el
@@ -7,24 +7,24 @@
 
 ;; exec-path-from-shell
 (use-package! exec-path-from-shell
+  :when IS-MAC
   :init
-  (when (or IS-LINUX IS-MAC)
-    (setq exec-path-from-shell-warn-duration-millis 2000)
-    (setq exec-path-from-shell-arguments '("-l"))
-    (setq exec-path-from-shell-variables '("LANG"
-                                           "LC_ALL"
-                                           "TERM"
-                                           "PATH"
-                                           "MANPATH"
-                                           "JAVA_HOME"
-                                           "JAVA_OPTS"
-                                           "SBT_OPTS"
-                                           "GOPATH"
-                                           "GOBIN"
-                                           "GO111MODULE"
-                                           "GOPROXY"))
-    ;; (setq exec-path-from-shell-debug t)
-    (exec-path-from-shell-initialize)))
+  (setq exec-path-from-shell-warn-duration-millis 2000)
+  (setq exec-path-from-shell-arguments '("-l"))
+  (setq exec-path-from-shell-variables '("LANG"
+                                         "LC_ALL"
+                                         "TERM"
+                                         "PATH"
+                                         "MANPATH"
+                                         "JAVA_HOME"
+                                         "JAVA_OPTS"
+                                         "SBT_OPTS"
+                                         "GOPATH"
+                                         "GOBIN"
+                                         "GO111MODULE"
+                                         "GOPROXY"))
+  ;; (setq exec-path-from-shell-debug t)
+  (exec-path-from-shell-initialize))
 
 (use-package! command-log-mode
   :commands global-command-log-mode
--- modules/my/tools/packages.el
+++ modules/my/tools/packages.el
@@ -1,7 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; my/tools/packages.el
 
-(package! exec-path-from-shell :pin "d14d6d2966efe5a1409f84a6b9d998268f74761d")
+(if IS-MAC
+    (package! exec-path-from-shell :pin "d14d6d2966efe5a1409f84a6b9d998268f74761d"))
 (package! command-log-mode :pin "af600e6b4129c8115f464af576505ea8e789db27")
 (when IS-LINUX
   (package! eaf :recipe (:host github :repo "manateelazycat/emacs-application-framework" :files ("*")))
