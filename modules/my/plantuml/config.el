cae27331829f6933211a4bfd84b4799cc6b6c866
Mike Chen <517926804@qq.com>
2021-04-08 22:12:59 +0800

    Improve plantuml

--- modules/my/plantuml/config.el
+++ modules/my/plantuml/config.el
@@ -1,4 +1,5 @@
 ;;; my/plantuml/config.el -*- lexical-binding: t; -*-
 
 (setq plantuml-jar-path (expand-file-name "plantuml.jar" doom-private-dir)
+      plantuml-output-type "png"
       org-plantuml-jar-path plantuml-jar-path)
