826dc77bf3ed40ae3baef32b142f2acffd1a1614
Mike Chen <517926804@qq.com>
2021-07-18 23:14:28 +0800

    Update ispell config

--- modules/my/spell/config.el
+++ modules/my/spell/config.el
@@ -2,7 +2,11 @@
 
 (after! ispell
   (setq ispell-dictionary "en_US")
-  (setq ispell-alternate-dictionary (expand-file-name "english-words" doom-private-dir)))
+  (setq ispell-alternate-dictionary (expand-file-name "english-words" doom-private-dir))
+
+  (defadvice! +ispell-lookup-words-a (orig &rest args)
+    :around #'ispell-lookup-words
+    (shut-up (apply orig args))))
 
 (when (featurep! :checkers spell)
   (after! nxml-mode
