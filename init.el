03400a70d8758797d4e2d57f3a4dfafca83721e2
Mike Chen <517926804@qq.com>
2021-07-31 12:43:34 +0800

    Synchronize the UI module from the Doom-Emacs module.

--- init.el
+++ init.el
@@ -22,23 +22,25 @@
         +prescient
         ;; +fuzzy
         +icons)
+       ;;vertico                        ; the search engine of the future
 
        :ui
        ;;deft                           ; notational velocity for Emacs
-       doom                            ; what makes DOOM look the way it does
-       ;;doom-dashboard                ; a nifty splash screen for Emacs
-       doom-quit                       ; DOOM quit-message prompts when you quit Emacs
-      (emoji +unicode)
-       hl-todo                         ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       doom                             ; what makes DOOM look the way it does
+       ;;doom-dashboard                 ; a nifty splash screen for Emacs
+       doom-quit                        ; DOOM quit-message prompts when you quit Emacs
+       (emoji +unicode)                 ; 🙂
+       hl-todo                          ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides                  ; highlighted indent columns
-       modeline                        ; snazzy, Atom-inspired modeline, plus API
-       nav-flash                       ; blink the current line after jumping
+       ;;ligatures                      ; ligatures and symbols to make your code pretty again
+       ;;minimap                        ; show a map of the code on the side
+       modeline                         ; snazzy, Atom-inspired modeline, plus API
+       nav-flash                        ; blink cursor line after big motions
        ;;neotree                        ; a project drawer, like NERDTree for vim
-       ophints                         ; highlight the region an operation acts on
-       (popup +all +defaults)          ; tame sudden yet inevitable temporary windows
-       ;;pretty-code                    ; replace bits of code with pretty symbols
-       ;;tabs                           ; an tab bar for Emacs
+       ophints                          ; highlight the region an operation acts on
+       (popup +all +defaults)           ; tame sudden yet inevitable temporary windows
+       ;;tabs                           ; a tab bar for Emacs
        (treemacs +lsp)                  ; a project drawer, like neotree but cooler
        ;;unicode                        ; extended unicode support for various languages
        vc-gutter                        ; vcs diff in the fringe
