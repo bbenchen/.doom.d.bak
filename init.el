c2658c833da90503fa1538741a5513df0c9a2c54
Mike Chen <517926804@qq.com>
2021-08-01 23:46:43 +0800

    Replace ivy module using vertico
    Add +org to mu4e

--- init.el
+++ init.el
@@ -18,11 +18,11 @@
        (company +childframe)            ; the ultimate code completion backend
        ;;helm                           ; the *other* search engine for love and life
        ;;ido                            ; the other *other* search engine...
-       (ivy                             ; a search engine for love and life
-        +prescient
-        ;; +fuzzy
-        +icons)
-       ;;vertico                        ; the search engine of the future
+       ;;(ivy                          ; a search engine for love and life
+       ;; +prescient
+       ;; ;; +fuzzy
+       ;; +icons)
+       (vertico +icons)                 ; the search engine of the future
 
        :ui
        ;;deft                           ; notational velocity for Emacs
@@ -167,7 +167,7 @@
        yaml                             ; JSON, but readable
 
        :email
-       mu4e                             ; WIP
+       (mu4e +org)                      ; WIP
        ;;notmuch                        ; WIP
        ;;(wanderlust +gmail)            ; WIP
 
