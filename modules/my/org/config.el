828570de5b6bb5db368bffc41b15c6440d70f1cb
Mike Chen <517926804@qq.com>
2021-08-01 23:35:31 +0800

    Delete org-link-beatify package

--- modules/my/org/config.el
+++ modules/my/org/config.el
@@ -10,9 +10,3 @@
       ;; (display-line-numbers-mode -1)
       ;; (org-indent-mode -1)
       (valign-mode +1))))
-
-(use-package! org-link-beautify
-  :config
-  (setq org-link-beautify-thumbnails-dir (concat doom-cache-dir "thumbnails/"))
-  (after! org
-    (add-hook! 'org-mode-hook #'org-link-beautify-mode)))
--- modules/my/org/packages.el
+++ modules/my/org/packages.el
@@ -4,5 +4,3 @@
 (package! valign
   :recipe (:host github :repo "casouri/valign")
   :pin "421c8c0cb4636e5fd87ac1022d6b8268c320f74f")
-
-(package! org-link-beautify :pin "40d40c1e6187ec9c13292beb3f7f319f189264d8")
