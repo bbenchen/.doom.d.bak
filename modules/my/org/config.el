d56f8e84f4aa52405d190bf5093af79f76742c68
Mike Chen <517926804@qq.com>
2021-09-10 16:22:17 +0800

    Add ditaa.jar

--- modules/my/org/config.el
+++ modules/my/org/config.el
@@ -1,7 +1,8 @@
 ;;; my/org/config.el -*- lexical-binding: t; -*-
 
 (after! org
-  (setq org-latex-pdf-process '("xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f")))
+  (setq org-latex-pdf-process '("xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f")
+        org-ditaa-jar-path (expand-file-name "ditaa.jar" doom-private-dir)))
 
 (use-package! valign
   :config
