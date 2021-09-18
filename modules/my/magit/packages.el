15e453c0a0d3c70edc65dcfff76755422839061f
Mike Chen <517926804@qq.com>
2021-09-18 22:29:01 +0800

    Use magit-svn for the default repository

--- modules/my/magit/packages.el
+++ modules/my/magit/packages.el
@@ -2,6 +2,4 @@
 ;;; my/magit/packages.el
 
 (if (executable-find "svn")
-  (package! magit-svn
-    :recipe (:host github :repo "cxb811201/magit-svn")
-    :pin "01af1e7c226d5b8db2c84f19e3c15e8d51a89d5b"))
+  (package! magit-svn :pin "350493217afdb7637564e089f475909adecd9208"))
