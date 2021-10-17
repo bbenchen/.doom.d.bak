14ecb85162cebe3c98977ad0a4d95a0fe8c739d8
Mike Chen <517926804@qq.com>
2021-10-17 20:01:30 +0800

    Bump :my chinese
    pin restclient

--- modules/my/chinese/packages.el
+++ modules/my/chinese/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; my/chinese/packages.el
 
-(package! rime :pin "1d9bcc6dad4182e9b6a5839f8261b260e57be2fc")
+(package! rime :pin "b296856c21d32e700005110328fb6a1d48dcbf8d")
 (package! posframe :pin "f97c4aff2c2c376ca62276d5597aa108546633a9")
--- packages.el
+++ packages.el
@@ -2,3 +2,5 @@
 ;;; ~/.doom.d/packages.el
 
 (unpin! (:completion company ivy) (:ui doom) (:emacs ibuffer) (:checkers spell) (:term vterm) (:tools magit debugger lsp) (:lang java scala))
+
+(package! restclient :pin "94d2e8421fa14d0e3307d70e1d1e2db9d43b2f95")
