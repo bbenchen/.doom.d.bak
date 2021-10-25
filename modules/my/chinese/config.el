439429698fa03d44449dac004eee657b7f7b1ed5
Mike Chen <517926804@qq.com>
2021-10-25 20:15:43 +0800

    Fix the bug that mu4e-compose sends emails using posframe to show rime input content

--- modules/my/chinese/config.el
+++ modules/my/chinese/config.el
@@ -24,19 +24,40 @@
       (if (fboundp 'rime--redisplay)
           (rime--redisplay))))
 
-  (custom-set-faces!
-    `(rime-default-face :foreground ,(doom-color 'modeline-fg) :background ,(doom-color 'modeline-bg)))
-
-  (if (fboundp 'rime--posframe-display-content)
-      (defadvice! +rime--posframe-display-content-a (args)
-        "给 `rime--posframe-display-content' 传入的字符串加一个全角空
+  (defadvice! +rime--posframe-display-content-filter-a (args)
+    "给 `rime--posframe-display-content' 传入的字符串加一个全角空
 格，以解决 `posframe' 偶尔吃字的问题。"
-        :filter-args #'rime--posframe-display-content
-        (cl-destructuring-bind (content) args
-          (let ((newresult (if (string-blank-p content)
-                               content
-                             (concat content "　"))))
-            (list newresult)))))
+    :filter-args #'rime--posframe-display-content
+    (cl-destructuring-bind (content) args
+      (let ((newresult (if (string-blank-p content)
+                           content
+                         (concat content "　"))))
+        (list newresult))))
+
+  (defadvice! +rime--posframe-display-content-a (content)
+    "显示可输入的内容，如果使用mu4e发送邮件时，使用popup显示，
+在mu4e发送邮件时，使用posframe显示输入内容存在BUG"
+    :override #'rime--posframe-display-content
+    (if (and (featurep 'posframe)
+             (display-graphic-p)
+             (not (or (derived-mode-p 'mu4e-compose-mode)
+                      (derived-mode-p 'org-msg-edit-mode))))
+        (if (string-blank-p content)
+            (posframe-hide rime-posframe-buffer)
+          (let* ((preedit (rime--current-preedit))
+                 (x (cond
+                     ((not rime-posframe-fixed-position) 0)
+                     ((not preedit) 0)
+                     ((not (overlayp rime--preedit-overlay)) 0)
+                     (t (rime--string-pixel-width preedit)))))
+            (apply #'posframe-show rime-posframe-buffer
+                   :string content
+                   :x-pixel-offset (- x)
+                   :background-color (doom-color 'modeline-bg)
+                   :foreground-color (doom-color 'modeline-fg)
+                   rime-posframe-properties)))
+      ;; Fallback to popup when not available.
+      (rime--popup-display-content content)))
 
   (map! (:map rime-mode-map
          (:when IS-MAC
