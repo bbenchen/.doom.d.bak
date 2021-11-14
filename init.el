5fc227cc2e96de148ef5301b80e2372e2556f17a
Mike Chen <517926804@qq.com>
2021-11-14 19:58:24 +0800

    Disable pdf module

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
        magit                            ; a git porcelain for Emacs
        make                             ; run make tasks from Emacs
        ;;pass                           ; password manager for nerds
-       (:if IS-MAC pdf)                 ; pdf enhancements
+       ;; (:if IS-MAC pdf)                 ; pdf enhancements
        ;;prodigy                        ; FIXME managing external services & code builders
        ;;rgb                            ; creating color strings
        ;;terraform                      ; infrastructure as code
