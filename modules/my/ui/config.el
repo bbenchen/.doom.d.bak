2523a8d12f082afd2b8822915426d58317acf124
Mike Chen <517926804@qq.com>
2022-03-26 09:28:55 +0800

    Delete the pixel-scroll package

--- modules/my/ui/config.el
+++ modules/my/ui/config.el
@@ -65,12 +65,7 @@
   )
 
 (if (boundp 'pixel-scroll-precision-mode)
-    (pixel-scroll-precision-mode t)
-  (use-package! good-scroll
-    :init
-    (add-hook! 'after-init-hook #'good-scroll-mode)
-    (global-set-key [remap next]  #'good-scroll-up-full-screen)
-    (global-set-key [remap prior] #'good-scroll-down-full-screen)))
+    (pixel-scroll-precision-mode t))
 
 ;; frame
 (add-hook! 'after-make-frame-functions #'(lambda (frame)
--- modules/my/ui/packages.el
+++ modules/my/ui/packages.el
@@ -2,6 +2,3 @@
 ;;; my/tools/packages.el
 
 (package! centered-cursor-mode :pin "4093821cc9759ca5a3c6e527d4cc915fc3a5ad74")
-
-(if (not (boundp 'pixel-scroll-precision-mode))
-    (package! good-scroll :pin "a7ffd5c0e5935cebd545a0570f64949077f71ee3"))
