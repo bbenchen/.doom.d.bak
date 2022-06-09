4b3d148595f3a0383119b340ee3d03a38d3f25cb
Mike Chen <517926804@qq.com>
2022-06-10 01:08:34 +0800

    Update eaf

--- modules/my/ui/packages.el
+++ modules/my/ui/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; my/tools/packages.el
 
-(package! centered-cursor-mode :pin "4093821cc9759ca5a3c6e527d4cc915fc3a5ad74")
+(package! centered-cursor-mode :pin "ebaeb80fba0bafdf6f95706308123dec2cf4b99f")
