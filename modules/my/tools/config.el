c60878b4d796e69235dc8fdd90e964f486141e45
Mike Chen <517926804@qq.com>
2022-06-14 01:11:23 +0800

    add pinentry package

--- modules/my/tools/config.el
+++ modules/my/tools/config.el
@@ -76,6 +76,13 @@
             (global-command-log-mode -1)
           (global-command-log-mode 1))))))
 
+(use-package! pinentry
+  :defer t
+  :init
+  (pinentry-start)
+  :config
+  (add-hook! 'kill-emacs-hook #'pinentry-stop))
+
 (use-package! eaf
   :load-path (lambda () (list (expand-file-name "lisp/eaf" doom-private-dir)))
   :when IS-LINUX
--- modules/my/tools/packages.el
+++ modules/my/tools/packages.el
@@ -5,5 +5,7 @@
     (package! exec-path-from-shell :pin "6336db9be13d46e2d4bc3b50bc37a3fbf30fdc9e"))
 (package! command-log-mode :pin "af600e6b4129c8115f464af576505ea8e789db27")
 
+(package! pinentry :pin "cd942f755c38a7ac270ce858bb887ebdd59edd26")
+
 (if IS-LINUX
     (package! epc :pin "e1bfa5ca163273859336e3cc89b4b6460f7f8cda"))
