d24645ce33a9f6ec9bb68c69cc7461f53b1cc5f8
Mike Chen <517926804@qq.com>
2022-07-01 12:49:58 +0800

    Add +jq flag for rest

--- init.el
+++ init.el
@@ -158,7 +158,7 @@
        ;;(python +lsp +pyright)         ; beautiful is better than ugly
        ;;qt                             ; the 'cutest' gui framework ever
        ;;racket                         ; a DSL for DSLs
-       rest                             ; Emacs as a REST client
+       (rest +jq)                       ; Emacs as a REST client
        ;;ruby                           ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust                           ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        (scala +lsp +tree-sitter)        ; java, but good
--- packages.el
+++ packages.el
@@ -5,4 +5,3 @@
 
 (if (featurep! :email mu4e +org)
     (package! org-msg :pin "60e22e446325a9b3387396459d98be7c1c52579d"))
-(package! restclient :pin "9e2cfa86529133eba6c9ef53794be182f15e4c21")
