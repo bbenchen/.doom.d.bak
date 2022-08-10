2fcc2fc4d270e3cea8f1bbfad0db5a1c52a658b2
Mike Chen <517926804@qq.com>
2022-08-10 23:14:34 +0800

    Use doom's built-in org-msg

--- packages.el
+++ packages.el
@@ -2,6 +2,3 @@
 ;;; ~/.doom.d/packages.el
 
 (unpin! (:editor multiple-cursors) (:checkers spell) (:term vterm) (:email mu4e))
-
-(if (featurep! :email mu4e +org)
-    (package! org-msg :pin "60e22e446325a9b3387396459d98be7c1c52579d"))
